049551617de6df55bc421d4831d8892afd684a15
yawkar <yawkarpov@gmail.com>
2022-08-24 13:00:04 +0300

    feat: add three new user contexts

---
+++ src/main/java/dev/yawkar/auxilium/context/ActiveContext.java
@@ -0,0 +1,13 @@
+package dev.yawkar.auxilium.context;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class ActiveContext extends AbstractContext {
+
+    @Override
+    public void handle(Update update) {
+
+    }
+}
