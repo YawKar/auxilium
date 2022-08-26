204f9313dadb9fd358d3553e8360ae9f75b5ee81
yawkar <yawkarpov@gmail.com>
2022-08-26 14:05:38 +0300

    feat: add ChatService and SessionService managing into ActiveContext

--- src/main/java/dev/yawkar/auxilium/context/ActiveContext.java
+++ src/main/java/dev/yawkar/auxilium/context/ActiveContext.java
@@ -2,7 +2,11 @@ package dev.yawkar.auxilium.context;
 
 import dev.yawkar.auxilium.bot.AuxiliumBot;
 import dev.yawkar.auxilium.exception.context.UnknownCommandException;
+import dev.yawkar.auxilium.repository.entity.Chat;
+import dev.yawkar.auxilium.repository.entity.Session;
+import dev.yawkar.auxilium.service.ChatService;
 import dev.yawkar.auxilium.service.ParserUtils;
+import dev.yawkar.auxilium.service.SessionService;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
@@ -16,11 +20,15 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public class ActiveContext extends AbstractContext {
 
     private final AuxiliumBot bot;
+    private final ChatService chatService;
+    private final SessionService sessionService;
     @Value("${activeContext.commands}")
     private String availableCommandsResponse;
 
-    public ActiveContext(@Lazy AuxiliumBot bot) {
+    public ActiveContext(@Lazy AuxiliumBot bot, ChatService chatService, SessionService sessionService) {
         this.bot = bot;
+        this.chatService = chatService;
+        this.sessionService = sessionService;
     }
 
     @SneakyThrows
@@ -32,12 +40,27 @@ public class ActiveContext extends AbstractContext {
                 default -> throw new UnknownCommandException();
             }
         } catch (RuntimeException e) {
-            bot.execute(new SendMessage(update.getMessage().getChatId().toString(), availableCommandsResponse));
+            if (update.getMessage().hasText() && !ParserUtils.isCommand(update.getMessage().getText())) {
+                exchangeMessage(update);
+            } else {
+                bot.execute(new SendMessage(update.getMessage().getChatId().toString(), availableCommandsResponse));
+            }
         }
     }
 
     @SneakyThrows
+    private void exchangeMessage(Update update) {
+        Chat chat = chatService.getChatById(update.getMessage().getChatId());
+        Session session = sessionService.getSessionById(chat.getSessionId());
+        long interlocutorId = chat.getId() == session.getHelperId() ? session.getRequesterId() : session.getHelperId();
+        bot.execute(new SendMessage(Long.toString(interlocutorId), update.getMessage().getText()));
+    }
+
+    @SneakyThrows
     private void runEndSession(Update update) {
+        Chat chat = chatService.getChatById(update.getMessage().getChatId());
+        chat.setContextType(ContextType.PASSIVE);
+        chatService.updateChat(chat);
         bot.execute(new SendMessage(update.getMessage().getChatId().toString(), availableCommandsResponse));
     }
 }
