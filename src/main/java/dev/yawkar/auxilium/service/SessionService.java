8416b30c1eca6a0ba2d46da08b690a451583a5a3
yawkar <yawkarpov@gmail.com>
2022-08-26 14:04:58 +0300

    feat: introduce SessionService for sessionRepository proxy

---
+++ src/main/java/dev/yawkar/auxilium/service/SessionService.java
@@ -0,0 +1,23 @@
+package dev.yawkar.auxilium.service;
+
+import dev.yawkar.auxilium.repository.SessionRepository;
+import dev.yawkar.auxilium.repository.entity.Session;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SessionService {
+
+    private final SessionRepository sessionRepository;
+
+    public SessionService(SessionRepository sessionRepository) {
+        this.sessionRepository = sessionRepository;
+    }
+
+    public Session getSessionById(long sessionId) {
+        return sessionRepository.findById(sessionId).get();
+    }
+
+    public Session updateOrSaveSession(Session session) {
+        return sessionRepository.save(session);
+    }
+}
